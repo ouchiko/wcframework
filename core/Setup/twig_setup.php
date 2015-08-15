853ecc46cc8a9ac63d5c41b72b96ffb11243bd1c
James Holden <ouchiko@gmail.com>
2015-08-15 18:16:40 +0100

    Updated

--- core/Setup/twig_setup.php
+++ core/Setup/twig_setup.php
@@ -1,6 +1,6 @@
 <?php
 	/** Twig **/
-	require_once __ROOT__."/core/vendor/Twig/lib/Twig/Autoloader.php";
+	require_once __ROOT__."/core/Vendor/Twig/lib/Twig/Autoloader.php";
 	Twig_Autoloader::register();
 	$loader = new Twig_Loader_Filesystem(__ROOT__."/core/MVC/views/");
 	$twig = new Twig_Environment($loader, array(
