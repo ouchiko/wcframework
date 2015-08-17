74e9c824c2ae86df354b8e29fdfa4a70b104c066
James <ouchiko@gmail.com>
2015-08-17 21:13:12 +0000

    Docs and formatting

--- core/MVC/controllers/PullController.php
+++ core/MVC/controllers/PullController.php
@@ -1,18 +1,24 @@
 <?php
-	
+/**
+* Class and Function List:
+* Function list:
+* - init()
+* Classes list:
+* - PullController extends Controller
+*/
 
 use AppSpace\Controllers\Controller;
 use AppSpace\Data\Mysql;
 
-
 class PullController extends Controller {
- 
-	public function init(){
-		passthru($_SERVER['DOCUMENT_ROOT']."/../git-pull");
-		$this -> view("system/pullrequest.html",array());
-	}
-
+    
+    /**
+     * Controller initialiser
+     * @return type
+     */
+    public function init() {
+        passthru($_SERVER['DOCUMENT_ROOT'] . "/../git-pull");
+        $this->view("system/pullrequest.html", array());
+    }
 }
-
-
 ?>
